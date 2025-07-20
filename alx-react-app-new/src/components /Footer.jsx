bbf269e47fd5d0b701c29caf3a1a3a838b30dd7a
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-07-20 11:54:28 +0200

    Delete alx-react-app-new/src/components  directory

--- alx-react-app-new/src/components /Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '15px', marginTop: '30px' }}>
-      <p>&copy; 2025 My Favorite Cities. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
