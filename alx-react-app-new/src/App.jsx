c2c8689be224c58d39b0d757068f26f1512f2541
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-07-20 14:15:36 +0200

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,14 +1,18 @@
+// src/App.jsx
+import React from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer';
-import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import Counter from './components/Counter'; // <- Add this line
 
 function App() {
   return (
     <div>
       <Header />
+      <UserProfile name="Lamla Mhlana" age={23} bio="Frontend dev from SA" />
       <MainContent />
-      <UserProfile name="Alice" age={30} bio="Frontend developer who loves exploring new cities." />
+      <Counter /> {/* <- Add this line */}
       <Footer />
     </div>
   );
