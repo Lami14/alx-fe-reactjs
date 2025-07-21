e18fab800e53cfc1e4543648c12067906c71ced0
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-07-21 11:24:27 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,44 +1,15 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RecipeList from './components/RecipeList'
-import AddRecipeForm from './components/AddRecipeForm'
-import useRecipeStore from './components/recipeStore';
-import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
+import React from "react";
+import AddRecipeForm from "./AddRecipeForm";
+import RecipeList from "./RecipeList";
 
 function App() {
-  const recipes = useRecipeStore(state => state.recipes);
-
   return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <>
-              <SearchBar />
-              <RecipeList />
-              <AddRecipeForm />
-              <FavoritesList />
-              <RecommendationsList />
-            </>
-          }
-        />
-        {recipes.map(recipe => {
-          const recipeId = recipe.id;
-          return (
-            <Route
-              path={`/${recipeId}`}
-              element={<RecipeDetails recipeId={recipeId} />}
-              key={recipeId}
-            />
-          )
-        })}
-      </Routes>
-    </BrowserRouter>
-  )
+    <div>
+      <h1>My Recipe App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
 }
 
-export default App
+export default App;
