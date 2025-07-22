0e2bb1283a97f59377335ba72842b560ae85ca20
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-07-22 14:24:30 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
-import AddRecipeForm from "./AddRecipeForm";
-import RecipeList from "./RecipeList";
+import { Link } from 'react-router-dom';
+import useRecipeStore from './recipeStore';
+import DeleteRecipeButton from './DeleteRecipeButton';
 
-function App() {
+function RecipeList() {
+  const recipes = useRecipeStore((state) => state.recipes);
+
   return (
     <div>
-      <h1>My Recipe App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <h1>Recipes</h1>
+      <Link to="/add">Add Recipe</Link>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+            {' | '}
+            <Link to={`/edit/${recipe.id}`}>Edit</Link>
+            {' | '}
+            <DeleteRecipeButton id={recipe.id} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default RecipeList; 
