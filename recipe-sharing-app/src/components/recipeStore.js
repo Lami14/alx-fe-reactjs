a5e9089be5e2f832b7829a86eda6234dd92a2a6e
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-07-22 13:03:00 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,11 +1,11 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, recipe],
-    })),
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
+  addRecipe: (recipe) => set((state) => ({
+    recipes: [...state.recipes, recipe]
+  })),
 }));
 
 export default useRecipeStore;
