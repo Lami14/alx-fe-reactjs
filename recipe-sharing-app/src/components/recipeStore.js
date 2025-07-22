698a1e590525934dbd1b5c6d9670e97594e09414
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-07-22 15:24:13 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,26 +1,13 @@
+// src/components/recipeStore.js
 import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
 
   setRecipes: (recipes) => set({ recipes }),
-
-  addRecipe: (recipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, recipe],
-    })),
-
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
-
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+  setSearchTerm: (term) => set({ searchTerm: term }),
 }));
 
-export default useRecipeStore;
+export default useRecipeStore; 
