c7a5dcdd6880e84f83bac3550b70771a83f9da7f
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-08-08 09:45:05 +0200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg 
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg 
                     hover:shadow-xl transition-transform duration-300 ease-in-out">
       <img
         src="https://via.placeholder.com/150"
